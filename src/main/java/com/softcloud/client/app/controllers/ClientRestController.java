081d63a0c75b4808fbf2a5aa931bb7049bbd99a5
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2020-03-09 16:58:55 -0600

    added crud methods

--- src/main/java/com/softcloud/client/app/controllers/ClientRestController.java
+++ src/main/java/com/softcloud/client/app/controllers/ClientRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,6 +40,24 @@ public class ClientRestController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Client create(@RequestBody Client client) {
 		return clienteService.save(client);
+	}
+	
+	@PutMapping("/clients/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client update(@RequestBody Client client, @PathVariable Long id) {
+		Client currentClient = clienteService.findById(id);
+		
+		currentClient.setLastName(client.getLastName());
+		currentClient.setName(client.getName());
+		currentClient.setEmail(client.getEmail());
+		
+		return clienteService.save(currentClient);
+	}
+	
+	@DeleteMapping("/clients/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
 	}
 
 
